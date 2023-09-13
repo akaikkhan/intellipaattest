4a7b88e5afef6263fde9e200666cd095e58badb1
awscodecommit <akaikkhan@yahoo.com>
2023-09-13 18:44:26 +0530

    simple test code for codedeploy

---
+++ simple.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Simple PHP Application</title>
+</head>
+<body>
+    <h1>Hello, World!</h1>
+    <p>This is a simple PHP application.</p>
+
+    <?php
+    // You can add PHP code here
+    $currentDate = date('Y-m-d H:i:s');
+    echo "<p>Current Date and Time: $currentDate</p>";
+    ?>
+</body>
+</html>
